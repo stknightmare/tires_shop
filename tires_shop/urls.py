07950e72b495749a910da64333d28c6cd9a26e5a
Stelios Al <stknightmare@hotmail.com>
2012-12-22 20:37:02 +0200

    init of shop

---
+++ tires_shop/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'tires_shop.views.home', name='home'),
+    # url(r'^tires_shop/', include('tires_shop.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
